855634316c64d03ccbbeb794a71a017be4eb98c3
Tomohiko Mukai <contact@mukai-lab.org>
2021-01-18 15:28:05 +0900

    initial commit

---
+++ Assets/Timer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // 追加
+
+public class Timer : MonoBehaviour
+{
+    float startTime;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        // キー押し始め
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            startTime = Time.time;
+        }
+        // キー放し
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            float diffTime = 10.0f - Mathf.Abs(Time.time - startTime);
+            AllSceneData.score = 100.0f - diffTime * 10.0f;
+            // スコアに応じてシーン切り替え
+            if (AllSceneData.score > 80)
+            {
+                SceneManager.LoadScene("Result");
+            }
+            else if (AllSceneData.score > 50)
+            {
+                SceneManager.LoadScene("Result");
+            }
+            else
+            {
+                SceneManager.LoadScene("Result");
+            }
+        }
+    }
+}
---
+++ Assets/TitleManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // 追加
+using UnityEngine.UI; // 追加
+
+public class TitleManager : MonoBehaviour
+{
+    public GameObject titleText;
+
+    // タイトルに戻ってきたときにベストスコアを更新し、現在のスコアをゼロクリア
+    void Start()
+    {
+        Text text = titleText.GetComponent<Text>();
+        if (AllSceneData.bestScore < AllSceneData.score)
+        {
+            AllSceneData.bestScore = AllSceneData.score;
+        }
+        text.text = "Best score: " + AllSceneData.bestScore.ToString();
+        AllSceneData.score = 0;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnPushStartButton()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+}
